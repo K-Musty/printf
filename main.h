171503c030071c51f9f56966cb5a0635a6980cf1
K-Musty <kmustapha9564@gmail.com>
2023-11-13 08:40:41 -0800

    man page

--- main.h
+++ main.h
@@ -2,7 +2,26 @@
 #define _MAIN_H_
 
 #include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
 
+/**
+ * struct outputt - Structure for each format output
+ *
+ * @output: each format representative
+ * @funct: function of format
+ *
+ */
+typedef struct ouputt
+{
+	char output;
+	int (*funct)(va_list);
+} outputt_t;
 
+int _putchar(
+
+int _printf(const char *format, ...);
 
 #endif
